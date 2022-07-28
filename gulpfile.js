35d6e78c46886d308b1881f5c566e3299e791c80
Josué Alarcón <josuealarco@gmail.com>
2022-07-28 16:13:19 -0500

    Adding Sass

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp= require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+function buildStyles() {
+    return gulp.src('./src/scss/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build/css'));
+};
+function watchers () {
+    gulp.watch('./src/sass/**/*.scss', buildStyles);
+    // gulp.watch('./src/images/**/*', convertWebp)
+}
+exports.default = gulp.parallel(buildStyles,watchers);
